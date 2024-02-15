c211a12145c2bde604f96c1a20f8310262b4ad8e
FaustoS88 <faustosaccoccio1988@gmail.com>
2024-02-15 12:49:31 +0100

    validacion form reg

--- js/register/register.js
+++ js/register/register.js
@@ -4,38 +4,38 @@ document.addEventListener("DOMContentLoaded", function() {
 
   function validarFormulario(evento) {
     evento.preventDefault();
-    let usuario = document.getElementById('firstName').value;
-    if(usuario.length == 0) {
+    let firstName = document.getElementById('firstName').value;
+    if(firstName.length == 0) {
       alert('No has escrito nada en el usuario');
       return;
     }
-    let usuario = document.getElementById('lastName').value;
-    if(usuario.length == 0) {
+    let lastName = document.getElementById('lastName').value;
+    if(lastName.length == 0) {
       alert('No has escrito nada en el usuario');
       return;
     }
-    let usuario = document.getElementById('nickname').value;
-    if(usuario.length == 0) {
+    let nickname = document.getElementById('nickname').value;
+    if(nickname.length == 0) {
       alert('No has escrito nada en el usuario');
       return;
     }
-    let usuario = document.getElementById('email').value;
-    if(usuario.length == 0) {
+    let email = document.getElementById('email').value;
+    if(email.length == 0) {
       alert('No has escrito nada en el usuario');
       return;
     }
-    let usuario = document.getElementById('confirmEmail').value;
-    if(usuario.length == 0) {
+    let confirmEmail = document.getElementById('confirmEmail').value;
+    if(confirmEmail.length == 0) {
       alert('No has escrito nada en el usuario');
       return;
     }
-    let clave = document.getElementById('password').value;
-    if (clave.length < 6) {
+    let clave1 = document.getElementById('password').value;
+    if (clave1.length < 6) {
       alert('La clave no es válida');
       return;
     }
-    let clave = document.getElementById('confirmPassword').value;
-    if(usuario. == 0) {
+    let clave2 = document.getElementById('confirmPassword').value;
+    if(clave2 != clave1 ) {
       alert('las contraseñas no coinciden');
       return;
     }
