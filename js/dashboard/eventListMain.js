2b8fa34b9e1fd5211a32d719dd5fd3e47fcf16c3
winston-majano <winstonmajano@gmail.com>
2024-02-23 18:32:28 +0100

    corrigiendo estructura del html del body en dashborad

--- js/dashboard/eventListMain.js
+++ js/dashboard/eventListMain.js
@@ -8,27 +8,29 @@ document.addEventListener('DOMContentLoaded', (event) => {
         getEvents().then((events) => {
 
             //  console.log(events[0].title)
-            let divContenedor = document.getElementById('eventsContent');
+            let divContenedor = document.getElementById('contenedorListado');
             let divContenido = document.getElementById('listEvent');
+            let listEventVip = document.getElementById('listEventVip');
+
 
             for (let evento = 0; evento < events.length; evento++) {
                 // console.log(events[evento].id_event)
 
                 let plantilla = `
-                <div id="divi" class="row ml-2 my-2 p-2 border border-success rounded">
-                <div class="col-4">
+                <div class="row p-1 my-1 mx-1  border border-success rounded">
+                <div class="col-4 col-md-4 col-lg-4">
                     <img class="img-fluid rounded" src="${events[evento].banner}" alt="nombre_evento">
                 </div>
-                <div class="col-8">
-                    <h5>${events[evento].day}</h3>
-                    <h3>${events[evento].title}</h3>
-                    <h5>${events[evento].address}</h3>
-                    <h5>${events[evento].address}</h3>
+                <div class="col-8 col-md-8 col-lg-8">
+                    <h6>${events[evento].day}</h6>
+                    <h6>${events[evento].title}</h6>
+                    <h6>${events[evento].address}</h6>
                 </div>
             </div>`;
 
                 divContenido.innerHTML += plantilla;
-                divContenedor.appendChild(divContenido);
+                divContenedor.insertBefore(divContenido, listEventVip)
+                //divContenedor.appendChild(divContenido);
 
             }
         })
