5822e763341b436804cb9838ca0d925ca78a312c
winston-majano <winstonmajano@gmail.com>
2024-02-26 13:43:07 +0100

    poniendo iconos en detelle de eventos

--- js/dashboard/eventListMain.js
+++ js/dashboard/eventListMain.js
@@ -4,14 +4,18 @@ import { getEvents } from '../../pages/events/eventsService.js'
 
 document.addEventListener('DOMContentLoaded', (event) => {
 
+let ID = undefined;
+
+
     function showEvents() {
         getEvents().then((events) => {
+
+           // console.log(events);
 
             //  console.log(events[0].title)
             let divContenedor = document.getElementById('contenedorListado');
             let divContenido = document.getElementById('listEvent');
             let listEventVip = document.getElementById('listEventVip');
-
 
             for (let evento = 0; evento < events.length; evento++) {
                 // console.log(events[evento].id_event)
@@ -22,21 +26,27 @@ document.addEventListener('DOMContentLoaded', (event) => {
                     <img class="img-fluid rounded" src="${events[evento].banner}" alt="nombre_evento">
                 </div>
                 <div class="col-8 col-md-8 col-lg-8">
-                    <h6>${events[evento].day}</h6>
-                    <h6>${events[evento].title}</h6>
-                    <h6>${events[evento].address}</h6>
+                    <h6><i class="fa-solid fa-calendar-days" style="color: #63E6BE;"></i> ${events[evento].day}</h6>
+                    <h4><i class="fa-solid fa-quote-left" style="color: #B197FC;"></i>${events[evento].title}</h4>
+                    <h6><i class="fa-solid fa-location-dot" style="color: #FFD43B;"></i> ${events[evento].address}</h6>
+                    <button type="button" class="btn btn-primary btn-sm">Ver mas</button>
                 </div>
+                
             </div>`;
-
+    
                 divContenido.innerHTML += plantilla;
                 divContenedor.insertBefore(divContenido, listEventVip)
                 //divContenedor.appendChild(divContenido);
-
             }
+
         })
     }
 
+  
+//TODO: ejecucion de funciones 
     showEvents()
-
 })
 
+
+
+   
--- landingPage.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-   let menuButton = document.getElementById('menu-button');
-    let menu = document.querySelector('.menu');
-  
-    menuButton.addEventListener('click', function () {
-      menu.classList.toggle('show-menu');
-    });
-  });
-  
