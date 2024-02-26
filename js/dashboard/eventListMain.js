fbc0e35f064f62aa0dea402b0f7590e4c9f20111
winston-majano <winstonmajano@gmail.com>
2024-02-26 14:18:52 +0100

    corrigiendo autocompletado en campos de correo y direccion del dashboard

--- js/dashboard/eventListMain.js
+++ js/dashboard/eventListMain.js
@@ -1,25 +1,13 @@
-
-
 import { getEvents } from '../../pages/events/eventsService.js'
 
 document.addEventListener('DOMContentLoaded', (event) => {
-
-let ID = undefined;
-
-
     function showEvents() {
         getEvents().then((events) => {
-
-           // console.log(events);
-
-            //  console.log(events[0].title)
             let divContenedor = document.getElementById('contenedorListado');
             let divContenido = document.getElementById('listEvent');
             let listEventVip = document.getElementById('listEventVip');
 
             for (let evento = 0; evento < events.length; evento++) {
-                // console.log(events[evento].id_event)
-
                 let plantilla = `
                 <div class="row p-1 my-1 mx-1  border border-success rounded">
                 <div class="col-4 col-md-4 col-lg-4">
@@ -33,20 +21,17 @@ let ID = undefined;
                 </div>
                 
             </div>`;
-    
+
                 divContenido.innerHTML += plantilla;
                 divContenedor.insertBefore(divContenido, listEventVip)
-                //divContenedor.appendChild(divContenido);
             }
-
         })
     }
 
-  
-//TODO: ejecucion de funciones 
+    //TODO: ejecucion de funciones 
     showEvents()
 })
 
 
 
-   
+
